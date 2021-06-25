596c8b3507c2fd418dac86fc348b79c1883f9839
Michael Raymond <michaelraymond@ATX-MRAYMOND.local>
2021-06-25 14:48:11 -0500

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || '8080'
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(parseInt(PORT,10), () => {
+    console.log('Listening for requests on http://localhost:${PORT}')
+})
+
